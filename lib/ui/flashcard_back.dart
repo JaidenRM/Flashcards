3b7cebce507e37a834e0a3fb3d9dac9ebd9e440b
Jaiden Muscat <jaidenreecemuscat@gmail.com>
2020-06-08 20:34:24 +1000

    -- android + ios splash screens added with a logo

--- lib/ui/flashcard_back.dart
+++ lib/ui/flashcard_back.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flashcards/bloc/man_fc_bloc.dart';
 import 'package:flashcards/bloc/state/update_fc_state.dart';
